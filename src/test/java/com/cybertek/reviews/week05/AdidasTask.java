695b3f4cbf86c8d1a636ea205b9752bdfe8fe7dc
SDETEMRE <dincsoyemre@gmail.com>
2022-01-09 19:39:57 +0300

    Adidas task added

--- src/test/java/com/cybertek/reviews/week05/AdidasTask.java
+++ src/test/java/com/cybertek/reviews/week05/AdidasTask.java
@@ -1,8 +1,12 @@
 package com.cybertek.reviews.week05;
 
 import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -11,9 +15,9 @@ import java.util.concurrent.TimeUnit;
 
 public class AdidasTask {
     WebDriver driver;
-
+    int expectedPurchaseAmount = 0;
     @BeforeMethod
-    public void setUp() {
+    public void setUp(){
         driver = WebDriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
@@ -21,20 +25,41 @@ public class AdidasTask {
     }
 
     @AfterMethod
-    public void tearDown() throws InterruptedException {
-        Thread.sleep(2000);
-        driver.quit();
-
+    public void tearDown(){
+        driver.close();
     }
 
     public void navigateTo(String str){
-        driver.findElement(By.linkText(str)).click();
+        driver.findElement(By.partialLinkText(str)).click();
     }
-    @Test
-    public void test(){
-        navigateTo("Laptops");
-        navigateTo("Sony vaio i5");
-        navigateTo("Add to chart");
 
+    public int productAdder(String category, String product){
+        navigateTo(category);
+        navigateTo(product);
+        // I want to get my expected price for that product
+        WebElement priceElement = driver.findElement(By.tagName("h3"));
+        String priceWholeText = priceElement.getText();
+        String[] arr = priceWholeText.split(" ");
+        int listPrice = Integer.parseInt(arr[0].substring(1));
+
+        navigateTo("Add to cart");
+        // handle pop up
+        WebDriverWait wait = new WebDriverWait(driver,10);
+        wait.until(ExpectedConditions.alertIsPresent());
+        Alert alert = driver.switchTo().alert();
+        alert.accept();
+        navigateTo("Home");
+        return listPrice;
     }
+
+    @Test
+    public void Test(){
+        // Navigate to "Laptop" → "Sony vaio i5" and click on "Add to cart". Accept pop up confirmation.
+        expectedPurchaseAmount += productAdder("Laptop","Sony vaio i5");
+        // Navigate to "Laptop" → "Dell i7 8gb" and click on "Add to cart". Accept pop up confirmation.
+        expectedPurchaseAmount += productAdder("Laptop","Dell i7 8gb");
+
+        System.out.println("expectedPurchaseAmount = " + expectedPurchaseAmount);
+    }
+
 }
