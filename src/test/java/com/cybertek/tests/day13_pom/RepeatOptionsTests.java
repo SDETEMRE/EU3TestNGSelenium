c73d513a5560b788afdbd313e6d6404cec9479f6
SDETEMRE <dincsoyemre@gmail.com>
2022-01-25 08:45:29 +0300

    extentreports added

--- src/test/java/com/cybertek/tests/day13_pom/RepeatOptionsTests.java
+++ src/test/java/com/cybertek/tests/day13_pom/RepeatOptionsTests.java
@@ -2,8 +2,15 @@ package com.cybertek.tests.day13_pom;
 
 import com.cybertek.pages.*;
 import com.cybertek.tests.TestBase;
+import com.cybertek.utilities.BrowserUtils;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class RepeatOptionsTests extends TestBase {
     /*VERIFY RADIO BUTTONS
@@ -36,10 +43,54 @@ public class RepeatOptionsTests extends TestBase {
 
         Assert.assertTrue(createCalendarEventsPage.days.isSelected(),"verify if selected");
         Assert.assertFalse(createCalendarEventsPage.weekday.isSelected(),"verify is not selected");
+    }
+      /*
+     VERIFY REPEAT OPTIONS
+        Open Chrome browser
+        Login as driver
+        Go to Activities->Calendar Events
+        Click on create calendar events button
+        Click on repeat checkbox
+        Verify that repeat options are Daily, Weekly, Monthly,Yearly(in this order)
+     */
+
+    @Test
+    public void test2() throws InterruptedException {
+
+        LoginPage loginPage = new LoginPage();
+        loginPage.loginAsDriver();
+
+        DashboardPage dashboardPage = new DashboardPage();
+        dashboardPage.navigateToModule("Activities", "Calendar Events");
+
+        CalendarEventsPage calendarEventsPage = new CalendarEventsPage();
+        calendarEventsPage.waitUntilLoaderScreenDisappear();
+        calendarEventsPage.createCalendarEvent.click();
+        CreateCalendarEventsPage createCalendarEventsPage = new CreateCalendarEventsPage();
+        Thread.sleep(2000);
+        createCalendarEventsPage.repeat.click();
+
+        Select repeatsDropdown = createCalendarEventsPage.repeatOptionsList();
+        List<String> expectedList = Arrays.asList("Daily","Weekly","Monthly","Yearly");
+        List<String> actualList = new ArrayList<>();
+        List<WebElement> actualOptions = repeatsDropdown.getOptions();
+
+        for (WebElement option : actualOptions) {
+            //get text of each element and add to actual List
+            System.out.println(option.getText());
+            actualList.add(option.getText());
+        }
+        System.out.println(actualList);
+        //ready method for getting text of list of web elements
+        List<String> actualList2 = BrowserUtils.getElementsText(actualOptions);
+        //Verify that repeat options are Daily, Weekly, Monthly,Yearly(in this order)
+        Assert.assertEquals(actualList,expectedList,"Verify Daily weekly monthly yearly");
+
 
 
 
 
 
     }
+
 }
