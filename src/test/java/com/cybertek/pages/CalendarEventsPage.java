84a644169fd82b23eca38d725a7850fdc54a839f
SDETEMRE <dincsoyemre@gmail.com>
2022-01-30 22:25:32 +0300

    week 7 review Data provider pom.xml  added

--- src/test/java/com/cybertek/pages/CalendarEventsPage.java
+++ src/test/java/com/cybertek/pages/CalendarEventsPage.java
@@ -12,4 +12,14 @@ public class CalendarEventsPage extends BasePage {
     @FindBy(css = "[title='Create Calendar event']")
     public WebElement createCalendarEvent;
 
+    @FindBy(css = "div[class='btn btn-link dropdown-toggle']")
+    public WebElement optionsLink;
+
+    @FindBy(css = "input[type='number']")
+    public WebElement pageNumber;
+
+    @FindBy(css = "button[class='btn dropdown-toggle ']")
+    public WebElement viewPerPage;
+
+
 }
